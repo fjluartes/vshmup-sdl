1d344e448b8f8119f2e84e2de2c51a19a96de9c7
Fred W11 <fjluartes@gmail.com>
2026-07-29 09:26:44 +0800

    06 adds bosses.c

---
+++ src/game/bosses.c
@@ -0,0 +1,95 @@
+/*
+ * bosses.c
+ */
+
+#include "common.h"
+
+#include "../game/bullets.h"
+#include "../game/effects.h"
+#include "../game/entities.h"
+#include "../system/atlas.h"
+#include "../system/draw.h"
+#include "../system/util.h"
+#include "bosses.h"
+
+extern App app;
+extern Entity *player;
+extern Stage stage;
+
+static void initGreenBoss(void);
+static void initYellowBoss(void);
+static void initBlueBoss(void);
+static void initRedBoss(void);
+static void tick(Entity *self);
+static void draw(Entity *self);
+static void takeDamage(Entity *self, int amount);
+static void die(Entity *self);
+static void fireGreenBossBullets(Entity *self);
+static void fireYellowBossBullets(Entity *self);
+static void fireBlueBossBullets(Entity *self);
+static void fireRedBossBullets(Entity *self);
+
+void initBoss(void)
+{
+
+}
+
+static void initGreenBoss(void)
+{
+
+}
+
+static void initYellowBoss(void)
+{
+
+}
+
+static void initBlueBoss(void)
+{
+
+}
+
+static void initRedBoss(void)
+{
+
+}
+
+static void tick(Entity *self)
+{
+
+}
+
+static void draw(Entity *self)
+{
+
+}
+
+static void takeDamage(Entity *self, int amount)
+{
+
+}
+
+static void die(Entity *self)
+{
+
+}
+
+static void fireGreenBossBullets(Entity *self)
+{
+
+}
+
+static void fireYellowBossBullets(Entity *self)
+{
+
+}
+
+static void fireBlueBossBullets(Entity *self);
+{
+
+}
+
+static void fireRedBossBullets(Entity *self)
+{
+
+}
