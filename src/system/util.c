1d01a61c617e2e520eafa9f768ba7cefb2a87928
Fred W11 <fjluartes@gmail.com>
2026-08-04 08:51:42 +0800

    07 edits util

--- src/system/util.c
+++ src/system/util.c
@@ -53,3 +53,20 @@ char* readFile(char* filename)
 
 	return buffer;
 }
+
+void calcSlope(int x1, int y1, int x2, int y2, double *dx, double *dy)
+{
+	int steps = MAX(abs(x1 - x2), abs(y1 - y2));
+
+	if (steps == 0)
+	{
+		*dx = *dy = 0;
+		return;
+	}
+
+	*dx = (x1 - x2);
+	*dx /= steps;
+
+	*dy = (y1 - y2);
+	*dy /= steps;
+}
